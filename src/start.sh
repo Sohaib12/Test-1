bc20ac599fb85c47a327ab55d01e5f60c3db58c2
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-22 20:33:25 +0200

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -3,6 +3,12 @@ g++ Test.cpp -o test
 mkdir go
 export GOPATH="/go"
 #export PATH=$PATH:$GOPATH/bin
+
+cd x86_64Linux2
+./autodock4
+
+cd ..
+
 go get -u github.com/ddollar/forego
 foreman check
 foreman start
