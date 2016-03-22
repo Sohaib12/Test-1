55292a0c2517a17e19cde1c692174bda6e1493e5
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-22 16:35:29 +0200

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 
+using namespace std;
 
 int main(int argc, char ** argv){
 
 	unsigned int num;
-	std::cin >> num;
+	
+	ifstream myReadFile;
+ 	myReadFile.open("asdf.txt");
+ 	if (myReadFile.is_open()) {
+ 		while (!myReadFile.eof()) {
+	    myReadFile >> num;
+	    cout<<output;
+	 }	
+	}
+	myReadFile.close();
 	for(int i=0; i<num; i++) {
 		std::cout << "Result is :" << i << std::endl;
 	}
