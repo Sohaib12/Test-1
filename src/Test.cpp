b4bd9df4da807f7462b4eee0012947f353152b9b
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-22 15:36:41 +0200

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
 #include <stdlib.h>
 
-#define CATCH_CONFIG_RUNNER
-#include "catch.hpp"
-
-unsigned int Factorial(unsigned int result) {
-	if (result == 1) {
-		return result;
-	}
-	result = Factorial(result-1) * result;
-}
-
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
 
 int main(int argc, char ** argv){
 
 	unsigned int num = 10;
-	unsigned int result = Factorial(num);
-
-	std::cout << "Result is :" << result << std::endl;
-	
+	std::cin >> num;
+	for(int i=0; i<num; i++) {
+		std::cout << "Result is :" << num  << std::endl;
+	}
 	return 0;
 }
