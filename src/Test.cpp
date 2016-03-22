9d8bd2edd7cf0c4481dbba24d30ec3349d8f7c92
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-22 15:39:47 +0200

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -7,7 +7,7 @@ int main(int argc, char ** argv){
 	unsigned int num = 10;
 	std::cin >> num;
 	for(int i=0; i<num; i++) {
-		std::cout << "Result is :" << num  << std::endl;
+		std::cout << "Result is :" << i << std::endl;
 	}
 	return 0;
 }
